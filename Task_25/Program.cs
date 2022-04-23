b33bb4b6519c845c7aaffc4954618ea8a255b47d
Andrey Evseev <a-evseev@yandex.ru>
2022-04-23 17:59:00 +0300

    написан и проверен код функции задания 25 для целых чисел

---
+++ Task_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите (функцию) цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B
+// Оба числа - целые
+
+int PowerNumberA(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+
+Console.Write("Ведите целое число, возводимое в степень: А = ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите целое число - степень, в которое возводится число А: B = ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int power;
+power = PowerNumberA(A, B);
+Console.WriteLine(power);
