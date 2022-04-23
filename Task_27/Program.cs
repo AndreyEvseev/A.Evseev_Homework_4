08386f00db4eac779dec65e010e338fa6f350391
Andrey Evseev <a-evseev@yandex.ru>
2022-04-23 19:01:49 +0300

    написан и проверен код функции задания 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите функцию, которая принимает на вход число 
+// и выдаёт сумму цифр в числе
+
+int SumNumber(int num)
+{
+    int sum0 = 0;
+    while(num >= 1)
+    {
+        sum0 = sum0 + num % 10;
+        num /= 10;
+    }
+    return(sum0);
+}
+
+
+Console.Write("Ведите целое число: А = ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum;
+sum = SumNumber(A);
+Console.WriteLine(sum);
